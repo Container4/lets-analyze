0e27601dcb67cd59e1c262c893831630ab56ddd6
Bill Tihen <btihen@gmail.com>
2018-04-29 06:33:31 +0200

    adjust formatting

--- app/models/plot.rb
+++ app/models/plot.rb
@@ -6,7 +6,8 @@ class Plot < ApplicationRecord
   validates :plot_name,         presence: true
   validates :plot_slope,        numericality: { only_integer: true }
   validates :plot_aspect,       numericality: { only_integer: true }
-  validates :plot_elevation_m,  presence: true, numericality: { only_integer: true }
+  validates :plot_elevation_m,  presence: true, numericality: {
+                                        only_integer: true }
   validates :plot_latitude,     presence: true, numericality: {
                                         less_than_or_equal_to:     90,
                                         greater_than_or_equal_to: -90 }
